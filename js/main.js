8fec469836eceac78d2b647e19a7884f6e5cea86
ens-csv <enrique.navarro64@gmail.com>
2026-02-16 17:52:28 -0600

    markers

--- js/main.js
+++ js/main.js
@@ -18,15 +18,15 @@ var cartoDB = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager
 }).addTo(map)
 
 var baseLayers={
-    'osm': osm,
-    'carto': cartoDB
+    'Open Street Map': osm,
+    'CartoDB Voyager': cartoDB    
 }
 L.control.layers(baseLayers).addTo(map)
 
 L.control.zoom({
     position:'topleft',
     zoomInText:'+',
-    zoomOutText:'-',
+    zoomOutText:'—',
     zoomInTitle: 'Acercar',
     zoomOutTitle:'Alejar'
 }).addTo(map)
@@ -37,3 +37,25 @@ L.control.scale({
     imperial:true,
     position:'bottomleft'
 }).addTo(map)
+
+var marcadorBolivia = L.marker([-16.495612, -68.133554]).addTo(map)
+
+var iconoPersonalizado = L.icon({
+    iconUrl:'img/frame.png',
+    //iconSize:[38,95]
+})
+
+var marcadorPersonalizado = L.marker([-16.517823, -68.217563],{
+    icon:iconoPersonalizado
+}).addTo(map)
+
+marcadorBolivia.bindPopup("<b>Hola La Paz</b><br>Sede de Bolivia").openPopup()
+
+marcadorPersonalizado.bindTooltip("Ubicación especial", {
+    permanent: false,
+    direction: 'top'
+})
+
+marcadorBolivia.on('click', function(e){
+    console.log('marcador clickeado', e.latlng)
+})
